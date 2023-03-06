f741028938faa1466883980d3787c51bfacc6d75
timothyshen <timothyshen1@hotmail.com>
2023-03-06 16:54:53 +0000

    feat: move alchemy to backend

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
+import { Alchemy, Network, Wallet, Utils } from 'alchemy-sdk';
+import { ethers } from 'ethers';
 
 @Injectable()
 export class EmailService {
@@ -19,8 +21,14 @@ export class EmailService {
   }
 
   async returnConfig() {
-    return {
-      privatekey: this.configService.get('PRIVATE_KEY'),
+    const settings = {
+      apiKey: this.configService.get('ALCHEMY_API_KEY'),
+      network: Network.ETH_GOERLI, // Replace with your network.
     };
+    const alchemy = new Alchemy(settings);
+    const privateKey = this.configService.get('PRIVATE_KEY');
+    const etherProvider = await alchemy.config.getProvider();
+    const wallet = new Wallet(privateKey, etherProvider);
+    return wallet;
   }
 }
